f4c0da0399dab891ed12fed211639323bca81fb0
yagoluis <yagoluisroberto@gmail.com>
2023-04-04 11:41:39 -0300

    feat: Returns all tweets posted by the user

--- src/main/java/com/tweteroo/api/controller/Tweets.java
+++ src/main/java/com/tweteroo/api/controller/Tweets.java
@@ -1,13 +1,17 @@
 package com.tweteroo.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.dto.TweetDTO;
+import com.tweteroo.api.model.Tweet;
 import com.tweteroo.api.service.TweetService;
 
 
@@ -18,9 +22,10 @@ public class Tweets {
     @Autowired
     private TweetService service;
     
-    @GetMapping
-    public String test() {
-        return "Deu certo !";
+    @GetMapping("/{username}")
+    public List<Tweet> getAllByUsername(@PathVariable String username) {
+
+        return service.getAllTweetsByUsername(username);
     }
 
     @PostMapping
--- src/main/java/com/tweteroo/api/service/TweetService.java
+++ src/main/java/com/tweteroo/api/service/TweetService.java
@@ -1,5 +1,7 @@
 package com.tweteroo.api.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +16,10 @@ public class TweetService {
 
     public void create(TweetDTO data) {
         repository.save(new Tweet(data));
+    }
+
+    public List<Tweet> getAllTweetsByUsername(String username) {
+
+        return repository.findByUsername(username);
     }
 }
